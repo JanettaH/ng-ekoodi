6596ff0e453a3a3056b889ac816af6395da52c91
JanettaH <janetta.huobonen@gmail.com>
2017-10-31 09:11:37 +0200

    Angular project base. Testing some stuff.

---
+++ src/app/user/user-list/user-list.component.ts
@@ -0,0 +1,35 @@
+import {Component, OnInit} from '@angular/core';
+import {User} from '../user';
+import {EventEmitter, Output} from '@angular/core';
+
+@Component({
+  selector: 'ekoodi-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+
+  title: string;
+  users: User[];
+  @Output() userSelected: EventEmitter<User>;
+
+  constructor() {
+    this.title = 'User List';
+    this.users = [];
+    this.userSelected = new EventEmitter();
+  }
+
+  ngOnInit() {
+    this.users = [
+      new User('Maija', 'Hämäläinen'),
+      new User('Pekka', 'Hämäläinen'),
+      new User('Ville', 'Hämäläinen')
+    ];
+  }
+
+    onUserSelect(user: User) {
+      this.userSelected.emit(user);
+      console.log(user);
+    }
+
+}
