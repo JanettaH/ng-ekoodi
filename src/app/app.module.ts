9f84c7117e95b144d581f8bfd89a2cae4980de2a
JanettaH <janetta.huobonen@gmail.com>
2017-11-06 10:15:52 +0200

    router and flex layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
-import { UserListComponent } from './user/user-list/user-list.component';
-import { UserInfoComponent } from './user/user-info/user-info.component';
+import {UserListComponent} from './user/user-list/user-list.component';
+import {UserInfoComponent} from './user/user-info/user-info.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatIconModule, MatInputModule, MatListModule, MatSliderModule} from '@angular/material';
 import {UserService} from './user/user.service';
+import {RouterModule, Routes} from '@angular/router';
+import {AddUserComponent} from './user/add-user/add-user.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
+const routes: Routes = [
+  {
+    path: 'add-user',
+    component: AddUserComponent
+  },
+  {
+    path: 'users',
+    component: UserListComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     UserListComponent,
-    UserInfoComponent
+    UserInfoComponent,
+    AddUserComponent
   ],
   imports: [
     BrowserModule,
@@ -23,11 +38,14 @@ import {UserService} from './user/user.service';
     MatInputModule,
     MatIconModule,
     MatSliderModule,
-    MatListModule
+    MatListModule,
+    RouterModule.forRoot(routes),
+    FlexLayoutModule
   ],
   providers: [
     UserService
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
